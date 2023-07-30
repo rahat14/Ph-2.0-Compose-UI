f5824812ebc742e890bec63dfc598ffb56514ef6
Thamina Omar Toma <omartoma33@gmail.com>
2023-07-30 13:51:58 +0600

    Camera Preview Added

---
+++ app/src/main/java/com/ph/syntex_error/phui/reels/ReelCameraPreview.kt
@@ -0,0 +1,87 @@
+package com.ph.syntex_error.phui.reels
+
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.LinearLayout
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.LifecycleOwner
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
+
+@Composable
+fun ReelCameraPreview(){
+
+    ExampleCameraPreview()
+
+}
+
+@Composable
+private fun ExampleCameraPreview() {
+
+    val context = LocalContext.current
+    val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+    val cameraController: LifecycleCameraController = remember { LifecycleCameraController(context) }
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding: PaddingValues ->
+        AndroidView(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            factory = { context ->
+                PreviewView(context).apply {
+                    setBackgroundColor(Color.White.toArgb())
+                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+                    scaleType = PreviewView.ScaleType.FILL_START
+                    implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+                }.also { previewView ->
+                    previewView.controller = cameraController
+                    cameraController.bindToLifecycle(lifecycleOwner)
+                }
+            },
+            onRelease = {
+                cameraController.unbind()
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun ExampleCameraScreen() {
+
+    val cameraPermissionState: PermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+
+    LaunchedEffect(key1 = Unit) {
+        if (!cameraPermissionState.status.isGranted && !cameraPermissionState.status.shouldShowRationale) {
+            cameraPermissionState.launchPermissionRequest()
+        }
+    }
+
+    if (cameraPermissionState.status.isGranted) {
+        // Permission is granted, we can show the camera preview
+        ExampleCameraPreview()
+    } else {
+        // In this screen you should notify the user that the permission
+        // is required and maybe offer a button to start another
+        // camera perission request
+//        NoCameraPermissionScreen(
+//            cameraPermissionState = cameraPermissionState
+//        )
+    }
+}
